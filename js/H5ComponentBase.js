0ca30a98c6f9f0429140b01e49627aae281696e6
fearlessfe <505380967@qq.com>
2017-07-11 17:01:12 +0800

    finish base component and h5 component

--- js/H5ComponentBase.js
+++ js/H5ComponentBase.js
@@ -1,2 +1,39 @@
 /* 基本图文组件对象 */
 
+var H5ComponentBase = function(name,cfg) {
+	var cfg = cfg || {};
+	var id = ('h5_c_'+Math.random()).replace('.','_');
+	var cls = ' h5_component_'+cfg.type;
+	var component = $('<div class="h5_component '+cls+' h5_component_name_'+name+' "id='+id+'>');
+
+	cfg.text   && component.text(cfg.text);
+	cfg.width  && component.width(cfg.width/2); //图片宽高与浏览器上显示的宽高
+	cfg.height && component.height(cfg.height/2);
+
+	cfg.css && component.css(cfg.css);
+
+	if( cfg.center === true){
+		component.css({
+			marginLeft: (cfg.width/4*-1)+'px',
+			left: '50%',
+
+		})
+	};
+
+
+	component.on('onLoad',function () {
+        component.addClass(cls+'_load').removeClass(cls+'_leave');
+        cfg.animateIn && component.animate( cfg.animateIn );
+        return false;//阻止事件冒泡
+    });
+
+    component.on('onLeave',function () {
+        component.addClass(cls+'_leave').removeClass(cls+'_load');
+        cfg.animateOut && component.animate( cfg.animateOut );
+        return false;//阻止事件冒泡
+    });
+
+	
+
+	return component;
+}
--- js/H5ComponentPoint.js
+++ js/H5ComponentPoint.js
@@ -1,2 +1,29 @@
 /* 散点图表组件对象 */
+var H5ComponentPoint = function(name , cfg){
+	var component = new H5ComponentBase(name , cfg);
 
+	var base = cfg.data[0][1];
+
+	$.each( cfg.data, function(index,item){
+		var point = $('<div class="point point_'+index+'">');
+
+		point.text(item[0]+'-'+item[1]);
+
+		var per = item[1]/base*100+'%';
+
+		point.width(per).height(per);
+
+		if(item[2]){
+			point.css('backgroundColor',item[2])
+		};
+
+		if(item[3] !== undefined &&item[4] !== undefined){
+			point.css('left',item[3]).css('top',item[4])
+		};
+
+
+
+		component.append(point);
+	});
+	return component;
+}
